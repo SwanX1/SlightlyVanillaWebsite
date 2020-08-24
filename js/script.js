5d5d2e1925da2e8a4e9dda37a099f83195727788
SwanX1 <36593309+SwanX1@users.noreply.github.com>
2020-08-24 21:16:51 +0300

    Added smooth auto-scroll from first view to rest of page & noscript warning

--- js/script.js
+++ js/script.js
@@ -1,10 +1,30 @@
 window.addEventListener('DOMContentLoaded', () => {
+    let onzeroscroll = false;
+    let onmainscroll = false;
+    let lastscroll = 0;
     document.querySelector('.main-arrow').classList.remove('hide-opacity');
-    document.body.onscroll = () => {
-        if (document.body.parentNode.scrollTop === 0) {
+    document.body.onscroll = e => {
+        console.log(lastscroll);
+        if (document.documentElement.scrollTop === 0) {
+            onzeroscroll = true;
             document.querySelector('.main-arrow').classList.remove('hide-opacity');
         } else {
-            document.querySelector('.main-arrow').classList.add('hide-opacity');
+            if (onzeroscroll) {
+                onzeroscroll = false;
+                document.querySelector('.main-arrow').classList.add('hide-opacity');
+                document.querySelector('#after-hero').scrollIntoView({ behavior: "smooth" });
+            }
         }
+        if (document.documentElement.scrollTop >= document.querySelector('#after-hero').offsetTop) {
+            onmainscroll = true;
+        } else {
+            if (onmainscroll) {
+                onmainscroll = false;
+                if (document.documentElement.scrollTop <= lastscroll) {
+                    document.body.scrollIntoView({ behavior: "smooth" });
+                }
+            }
+        }
+        lastscroll = document.documentElement.scrollTop;
     };
 });
