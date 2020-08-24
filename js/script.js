2e6a9740aaff31bb7c072fe734273c552af43e83
SwanX1 <36593309+SwanX1@users.noreply.github.com>
2020-08-24 18:04:08 +0300

    minor changes

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 window.addEventListener('DOMContentLoaded', () => {
     document.body.onscroll = () => {
-        document.querySelector('.main-arrow').classList.add('hide');
+        document.querySelector('.main-arrow').classList.add('hide-opacity');
         document.body.onscroll = null;
     };
 });
